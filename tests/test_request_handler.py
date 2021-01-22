3e72f4b83e426d9824ea2b1f82774a8d73d89b6a
Gleison Batista Soares <gleisonbs@gmail.com>
2021-01-22 20:19:06 -0300

    feat: ensure RequestHandler has a add_route attribute

--- tests/test_request_handler.py
+++ tests/test_request_handler.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 from restful_serverless import request_handler
+from restful_serverless.request_handler import RequestHandler
 
 
 class TestRequestHandler(TestCase):
@@ -9,4 +10,11 @@ class TestRequestHandler(TestCase):
             hasattr(request_handler, "RequestHandler"),
             True,
             "request_handler has no RequestHandler attribute",
+        )
+
+    def test_request_handler_has_add(self):
+        self.assertEqual(
+            hasattr(RequestHandler, "add_route"),
+            True,
+            "RequestHandler has no add_route attribute",
         )
